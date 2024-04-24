a67d0abd2619108407a8ae9835a1ba0de1eae9ca
ezecon <nationhexus@gmail.com>
2024-04-24 16:16:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+
+function App() {
+
+  return <div className='text-red-500 font-2xl'>Test my app</div>;
+}
+
+export default App;
---
+++ src/NewComponant/NewComponant.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+
+function App() {
+
+  return <div className='text-red-500 font-2xl'>Fake Div</div>;
+}
+
+export default App;
